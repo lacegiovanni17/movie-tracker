b28240dc3eb93af1c96ccdaf1f3695c493038c4d
Mac <mac@Macs-MacBook-Air.local>
2025-05-12 01:19:18 +0100

    edited .gitignore file

--- MovieTrackerBackend/obj/Debug/net9.0/MovieTrackerBackend.AssemblyInfo.cs
+++ MovieTrackerBackend/obj/Debug/net9.0/MovieTrackerBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieTrackerBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d01acbcb8cffb42fdb4a580f5a0161bb8ce379e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieTrackerBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieTrackerBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
